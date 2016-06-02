d0960d26f3ca01d441657b6bc22cadd842c05cec
Mullink3 <mullink3@gmail.com>
2016-06-01 21:19:54 -0500

    Create Event Activities

---
+++ app/src/main/java/com/nonvoid/andromeda/SubmittingAHintPerLocationActivity.java
@@ -0,0 +1,13 @@
+package com.nonvoid.andromeda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmittingAHintPerLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submitting_ahint_per_location);
+    }
+}
