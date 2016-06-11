89efac2170048e749fc6c2def3964bdca7343157
Mullink3 <mullink3@gmail.com>
2016-06-11 14:19:59 -0500

    Deleted activities beside main activitiy and MVP folder activitiies

--- app/src/main/java/com/nonvoid/andromeda/MVP/ViewHintsActivity.java
+++ app/src/main/java/com/nonvoid/andromeda/MVP/ViewHintsActivity.java
@@ -1,7 +1,11 @@
 package com.nonvoid.andromeda.MVP;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 import com.nonvoid.andromeda.R;
 
@@ -11,8 +15,17 @@ public class ViewHintsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_hints);
-
-
-
     }
+
+public void onSaveHintLocation (View view)
+{
+
+
+
+    //EDIT THIS INTENT TO START A DIFFERENT ACTIVITY
+    Intent startNextAcivity = new Intent(ViewHintsActivity.this, MapsActivity.class);
+    startActivity(startNextAcivity);
+}
+
+
 }
